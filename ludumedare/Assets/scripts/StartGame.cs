8a8ac3a23fc6587d53a780d2f2121ea8428ad6ed
thomas-HPwin7\thomas2 <thomas.e.gruenewald@gmail.com>
2016-08-28 16:14:22 -0600

    added title scene

---
+++ ludumedare/Assets/scripts/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void click() {
+		Debug.Log ("start the game");
+		//SceneManager.LoadScene ("scene2");
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
