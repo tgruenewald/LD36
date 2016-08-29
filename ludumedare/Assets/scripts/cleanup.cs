fe2d72dc863534329cc5e3789ba034922a415260
tgruenewald <terrygruenewald@gmail.com>
2016-08-28 23:06:06 -0600

    scenes for added happy path battles

---
+++ ludumedare/Assets/scripts/cleanup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class cleanup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i< Player.InventoryNumber; i++) {
+			Button inventorySlot = GameObject.Find ("InventoryButton" + (i+1)).GetComponent<UnityEngine.UI.Button> ();
+
+			inventorySlot.tag = "available";
+			inventorySlot.image.sprite = Resources.Load<Sprite>("Sprites/inventorycell");
+			if (i >= Player.MAX_INVENTORY) {
+				i = 0;
+			}			
+		}
+		Player.InventoryNumber = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
